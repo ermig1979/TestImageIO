98988c6e3e03848fbc5f6ccf55e8032cf896dfdf
Yermalayeu Ihar <ermig@tut.by>
2021-12-28 18:30:58 +0300

    +add stb; add Compressed structure and Framework abstract class.

--- src/TestImageIO/TestImageIO.cpp
+++ src/TestImageIO/TestImageIO.cpp
@@ -1,94 +1,111 @@
-#include <iostream>
-#include <iomanip>
-
-#include <sys/time.h>
-
-#include "Simd/SimdView.hpp"
+/*
+* Test Image IO Project (http://github.com/ermig1979/TestImageIO).
+*
+* Copyright (c) 2021-2021 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "TestImageIO/Common.h"
 
 #include <turbojpeg.h>
 
-typedef Simd::View<Simd::Allocator> View;
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-inline int64_t TimeUsec()
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+namespace Test
 {
-	timeval t;
-	gettimeofday(&t, NULL);
-	return int64_t(t.tv_sec) * 1000000 + t.tv_usec;
-}
-
-void TestSimd(const View& src, int quality)
-{
-	uint8_t* data = NULL;
-	size_t size = 0;
-
-	data = SimdImageSaveToMemory(src.data, src.stride, src.width, src.height, (SimdPixelFormatType)src.format, SimdImageFileJpeg, quality, &size);
-
-	if (data)
-		SimdFree(data);
-	else
-		std::cout << "Simd can't save image to memory!" << std::endl;
-}
-
-void TestTurboJpeg(tjhandle _jpegCompressor, const View& src, int quality, const std::string & name)
-{
-	long unsigned int _jpegSize = 0;
-	uint8_t* _compressedImage = NULL;
-
-	tjCompress2(_jpegCompressor, src.data, src.width, src.stride, src.height, TJPF_RGB,
-		&_compressedImage, &_jpegSize, quality <= 90 ? TJSAMP_420 : TJSAMP_444, quality, TJFLAG_FASTDCT);
-
-	if (!name.empty())
+	void TestSimd(const Image& src, int quality)
 	{
-		FILE* file = fopen(name.c_str(), "wb");
-		if (!file) 
-		{
-			std::cout << "Could not open JPEG file: " << std::endl;
-			return;
-		}
-		if (fwrite(_compressedImage, _jpegSize, 1, file) < 1) 
-		{
-			std::cout << "Could not write JPEG file: " << std::endl;
-			return;
-		}
-		fclose(file);
+		uint8_t* data = NULL;
+		size_t size = 0;
+
+		data = SimdImageSaveToMemory(src.data, src.stride, src.width, src.height, (SimdPixelFormatType)src.format, SimdImageFileJpeg, quality, &size);
+
+		if (data)
+			SimdFree(data);
+		else
+			std::cout << "Simd can't save image to memory!" << std::endl;
 	}
 
-	if (*_compressedImage)
-		tjFree(_compressedImage);
-	else
-		std::cout << "TurboJpeg can't save image to memory!" << std::endl;
-}
-
-bool Test(const std::string& name, const int count, int quality, tjhandle jpegCompressor)
-{
-	View src;
-	if (!src.Load(std::string("../data/image/") + name, View::Rgb24))
+	void TestTurboJpeg(tjhandle _jpegCompressor, const Image& src, int quality, const String& name)
 	{
-		std::cout << "Can't load test image!" << std::endl;
-		return false;;
+		long unsigned int _jpegSize = 0;
+		uint8_t* _compressedImage = NULL;
+
+		tjCompress2(_jpegCompressor, src.data, src.width, src.stride, src.height, TJPF_RGB,
+			&_compressedImage, &_jpegSize, quality <= 90 ? TJSAMP_420 : TJSAMP_444, quality, TJFLAG_FASTDCT);
+
+		if (!name.empty())
+		{
+			FILE* file = fopen(name.c_str(), "wb");
+			if (!file)
+			{
+				std::cout << "Could not open JPEG file: " << std::endl;
+				return;
+			}
+			if (fwrite(_compressedImage, _jpegSize, 1, file) < 1)
+			{
+				std::cout << "Could not write JPEG file: " << std::endl;
+				return;
+			}
+			fclose(file);
+		}
+
+		if (*_compressedImage)
+			tjFree(_compressedImage);
+		else
+			std::cout << "TurboJpeg can't save image to memory!" << std::endl;
 	}
 
-	std::cout << "Test (name = " << name << "\t, quality = " << quality << ") :";
+	bool Test(const String & name, const int count, int quality, tjhandle jpegCompressor)
+	{
+		Image src;
+		if (!src.Load(String("../data/image/") + name, Image::Rgb24))
+		{
+			std::cout << "Can't load test image!" << std::endl;
+			return false;
+		}
 
+		std::cout << "Test (name = " << name << "\t, quality = " << quality << ") :";
 
-	int64_t startSimd = TimeUsec();
-	for (int i = 0; i < count; ++i)
-		TestSimd(src, quality);
-	int64_t endSimd = TimeUsec();
-	double timeSimd = double(endSimd - startSimd) * 0.001 / count;
+		int64_t startSimd = Cpl::TimeCounter();
+		for (int i = 0; i < count; ++i)
+			TestSimd(src, quality);
+		int64_t endSimd = Cpl::TimeCounter();
+		double timeSimd = Cpl::Miliseconds(endSimd - startSimd) / count;
 
-	int64_t startTj = TimeUsec();
-	for (int i = 0; i < count; ++i)
-		TestTurboJpeg(jpegCompressor, src, quality, "");
-	int64_t endTj = TimeUsec();
-	double timeTj = double(endTj - startTj) * 0.001 / count;
+		int64_t startTj = Cpl::TimeCounter();
+		for (int i = 0; i < count; ++i)
+			TestTurboJpeg(jpegCompressor, src, quality, "");
+		int64_t endTj = Cpl::TimeCounter();
+		double timeTj = Cpl::Miliseconds(endTj - startTj) / count;
 
-	std::cout << std::setprecision(3) << std::fixed << "TurboJpeg : " << timeTj << ", Simd : " << timeSimd << " ms, TurboJpeg / Simd : " << timeTj / timeSimd << std::endl;
+		std::cout << std::setprecision(3) << std::fixed << "TurboJpeg : " << timeTj << ", Simd : " << timeSimd << " ms, TurboJpeg / Simd : " << timeTj / timeSimd << std::endl;
 
-	src.Save(std::string("simd_") + std::to_string(quality) + "_" + name, SimdImageFileJpeg, quality);
-	TestTurboJpeg(jpegCompressor, src, quality, std::string("tj_") + std::to_string(quality) + "_" + name);
+		src.Save(std::string("simd_") + std::to_string(quality) + "_" + name, SimdImageFileJpeg, quality);
+		TestTurboJpeg(jpegCompressor, src, quality, String("tj_") + std::to_string(quality) + "_" + name);
 
-	return true;
+		return true;
+	}
 }
 
 int main()
@@ -98,10 +115,10 @@ int main()
 
 	for (int i = 0; i < 5; ++i)
 	{
-		Test("city.jpg", N, qulities[i], jpegCompressor);
-		Test("face.jpg", N, qulities[i], jpegCompressor);
-		Test("forest.jpg", N, qulities[i], jpegCompressor);
-		Test("text.png", N, qulities[i], jpegCompressor);
+		Test::Test("city.jpg", N, qulities[i], jpegCompressor);
+		Test::Test("face.jpg", N, qulities[i], jpegCompressor);
+		Test::Test("forest.jpg", N, qulities[i], jpegCompressor);
+		Test::Test("text.png", N, qulities[i], jpegCompressor);
 	}
 
 	tjDestroy(jpegCompressor);
